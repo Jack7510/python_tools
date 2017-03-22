192f7aef5e9186655ffc1b17e5f15c9ea4be066f
admin <admina@bindeiMac.local>
2017-03-22 09:50:40 +0800

    test framework python script

---
+++ test_framework/test_host.py
@@ -0,0 +1,25 @@
+#!/local/bin/env python
+# file: test_host.py
+# auth: Jack Lee
+# Date: Mar 20, 2017
+
+import os
+import subprocess
+import json
+
+
+def exec_adb_cmd(tid, json_param):
+    return
+
+
+
+
+# test program
+if __name__ == '__main__':
+    print "hello world!"
+
+    # build up json parameters
+
+    # execute 'adb shell test_proxy' to send test command
+
+    # verify test result
